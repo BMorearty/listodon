4daa5c8fea7ebb0ee0f910408c38fa47dea071e3
Brian Morearty <brian@morearty.org>
2023-01-05 22:54:19 -0800

    Don't delete Google Analytics cookies.

--- src/routes/reset/+page.svelte
+++ src/routes/reset/+page.svelte
@@ -14,7 +14,9 @@
       const cookie = cookies[i];
       const eqPos = cookie.indexOf('=');
       const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
-      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+      if (!name.startsWith('_ga')) {
+        document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+      }
     }
   }
 </script>
