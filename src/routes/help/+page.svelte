8d87b92d409fc9723ec7821889e432b8e5db6add
Brian Morearty <brian@morearty.org>
2023-01-06 09:03:01 -0800

    Update the help

--- src/routes/help/+page.svelte
+++ src/routes/help/+page.svelte
@@ -1,22 +1,31 @@
 <h1>How to use Listodon</h1>
 <p>
   If you have multiple distinct interests—e.g. politics and tech—and you follow people in both
-  arenas, Mastodon will intermingle their posts.
+  arenas, Mastodon will intermingle their posts. If you don’t like the intermingling, you have a
+  couple options:
 </p>
+<ol>
+  <li>
+    Make accounts on different servers, e.g. a politics server and a tech server. Follow different
+    people from different accounts.
+  </li>
+  <li>
+    Make Lists for your topics of interest: a Politics list and a Tech list. Whenever you follow
+    someone you add them to one of your lists. (Mastodon doesn’t notify them.)
+  </li>
+</ol>
 <p>
-  If you don’t like the intermingling, you can make Lists for your topics of interest: a Politics
-  list and a Tech list. Whenever you follow someone you can add them to one of your lists. (Mastodon
-  doesn’t notify them.)
-</p>
-<p>
-  But what if you follow people and forget to add them to a list? Listodon will tell you. The first
-  time you run it, it asks for permission to look at your account. Then it looks at everyone you
-  follow, and makes sure you’ve remembered to put them all in lists. It shows you everyone you
-  haven’t put in a list.
+  Listodon is for option 2. What if you follow people and forget to add them to a list? Listodon
+  will tell you. The first time you run it, it asks for permission to look at your account. Then it
+  looks at everyone you follow, and makes sure you’ve remembered to put them all in lists. It shows
+  you everyone you haven’t put in a list.
 </p>
 <p>How do you fix it? Follow the link to the person’s profile on Mastodon and click:</p>
 <img src="help.png" class="helpimage" alt="In Mastodon, click Add or Remove from lists" />
 <p>You can come back to Listodon any time you want and re-check.</p>
+<div class="madeby">
+  Made with ❤️ by <a href="https://techhub.social/@BMorearty">😱 Brian Morearty</a>
+</div>
 
 <style>
   .helpimage {
@@ -24,5 +33,9 @@
     display: block;
     margin-left: auto;
     margin-right: auto;
+  }
+  .madeby {
+    margin-top: 20px;
+    text-align: right;
   }
 </style>
