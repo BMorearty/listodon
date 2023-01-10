1d3fe436b1ddbc7008646ac77c6fa5894f0625d2
Brian Morearty <brian@morearty.org>
2023-01-10 11:49:00 -0800

    force deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import importAssets from 'svelte-preprocess-import-assets';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
