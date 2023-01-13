2eb37f431b77cffadc70d3ed498dd1d1ef2fc0a4
Brian Morearty <brian@morearty.org>
2023-01-13 11:59:15 -0800

    Don't ssr the homepage because the Mastodon picture will flash if the form needs to be shown.

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,8 @@
 import type { PageLoad } from './$types';
 
+// Don't ssr this page becuase it will flash the Mastodon picture for a moment.
+export const ssr = false;
+
 export const load: PageLoad = ({ fetch }) => {
   return { fetch };
 };
